6f9139d1feda9907047d179bd2647cb2f4b150ae
sweetje5us <82095847+sweetje5us@users.noreply.github.com>
2022-06-07 17:47:42 +0500

    сщь

--- examples/secondTest.spec.cy.js
+++ examples/secondTest.spec.cy.js
@@ -1,10 +1,19 @@
 describe('visit url', () => {
   it('valid link', () => {
+    // const user = {
+    //   telephone,
+    //   code,
+    //   user_id,
+    //   user_token
+    // }
     cy.visit('/')
     cy.log(`Logging in as `)
     cy.get('[data-cy="auth-page-phone-input__input"]')
-      .type('9638700821')
-      cy.get('[data-cy="auth-page-code-input__input"]')
-      .type('4755')
+      .type('9638700823'),
+      function(){
+       cy.get('[data-cy="auth-page-code-input__input"]')
+      .type(prompt("code"), '')
+      }
+  
   })
 })
