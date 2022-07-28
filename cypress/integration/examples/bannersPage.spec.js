f3f55398e2e4edf62199e3b768535838adfc730b
sweetje5us <82095847+sweetje5us@users.noreply.github.com>
2022-07-28 20:33:04 +0500

    3

--- cypress/integration/examples/bannersPage.spec.js
+++ cypress/integration/examples/bannersPage.spec.js
@@ -1,8 +1,9 @@
 describe('Banners', ()=>{
     before(()=>{
-        cy.visitCrmPage('9638700821', 'employee',"dev", "administration")
+        cy.visitCrmPage('9638700821', 'employee',"dev", "administration?tab=%D0%91%D0%B0%D0%BD%D0%BD%D0%B5%D1%80%D1%8B")
     })
 it('First case', ()=>{
-
+    cy.get('.add-bunner-button').click()
+    cy.get('[data-cy="create-banner-name-input"] > .field__input-wrapper > [data-cy="input"]').type('title')
     })
 })
